29ad3d7a268fdc0bb16537671e9beba985104199
Shimul Chowdhury <shimulblogs@gmail.com>
2015-05-08 08:33:15 +0600

    Refreshed again

--- main.c
+++ main.c
@@ -18,22 +18,33 @@ int main(int argvc, char* argv[])
 
     if(argvc < 2) show_error_and_exit(SOURCE_NOT_PROVIDED, 0);
 
+    if(argvc >= 3){
+        if(strcmp(argv[2], "-D")){
+            debug = 1;
+        }else{
+            debug = 0;
+        }
+    }
+
+
     read_file(argv[1]);
 
-    debug = 0;
+
+
     //write token array
-
-    printf( "Content-type: text/html\n\n");
-
-    strcpy(token_array_file_name, argv[1]);
-    strcpy(syntax_tree_file_name, argv[1]);
-    strcat(token_array_file_name, ".tokens.txt");
-    strcat(syntax_tree_file_name, ".ast.txt");
-    remove( syntax_tree_file_name );
-    write_token_array();
+    if(debug){
+        strcpy(token_array_file_name, argv[1]);
+        strcpy(syntax_tree_file_name, argv[1]);
+        strcat(token_array_file_name, ".tokens.txt");
+        strcat(syntax_tree_file_name, ".ast.txt");
+        remove( syntax_tree_file_name );
+        write_token_array();
+    }
     parse();
     free_token_array(&token_array);
-    write_syntax_tree(ast_root, 0);
+    if(debug){
+        write_syntax_tree(ast_root, 0);
+    }
     interpret();
     return 0;
 }
